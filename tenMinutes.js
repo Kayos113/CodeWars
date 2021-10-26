fb6c94ca82188e90a02c9e7b150c03905ce9b445
Knapp <williamknapp@rockypoint.k12.ny.us>
2021-10-26 09:08:27 -0500

    5/13/2022 practice 2

---
+++ tenMinutes.js
@@ -0,0 +1,14 @@
+// given string, directional characters 'n','s', 'w', 'e'
+// each character indicates 1 block walked, which takes one minute
+// determine if a walk takes 10 minutes
+// also determine if the walk will return you to the starting location
+
+function isValidWalk(walk) {
+  // array - hold x, y value pair to compute how far from starting value we are
+  // variable - int - count minutes taken in walk
+  // loop - walk, character by character, evaluating both time, and positoinally where we are
+  // exit loop
+  // check if the walk time is 10 minutes, if not return false
+  // check if the positional value is 0,0 or back at the starting point., if not return false
+  // return true, as all conditions have been met
+}

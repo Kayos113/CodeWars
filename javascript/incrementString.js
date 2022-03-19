4e29f0c5a5ca84d797efa9fe84e7e18deab4211b
William Knapp <willyknapp@gmail.com>
2022-03-18 19:12:18 -0500

    Daily Codewars Exercise

---
+++ javascript/incrementString.js
@@ -0,0 +1,15 @@
+function incrementString (strng) {
+  let index = strng.length;
+  while(!isNaN(parseInt(strng.substring(index-1)))) {
+    console.log(parseInt(strng.substring(index-1)));
+    index--;
+  }
+  let subString = strng.substring(0,index);
+  let num = (parseInt(strng.substring(index))+1);
+  let numString = !isNaN(num) ? num+'' : '1';
+  let zeros = '';
+  while(subString.length+numString.length+zeros.length<strng.length) {
+    zeros+='0';
+  }
+  return subString+zeros+numString;
+}

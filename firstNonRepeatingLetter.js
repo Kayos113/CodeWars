b04144621b945c3a2a4e6afd2e16594ed4db5db5
William Knapp <willyknapp@gmail.com>
2022-04-11 02:19:19 -0500

    Codewar Daily Practice

--- firstNonRepeatingLetter.js
+++ firstNonRepeatingLetter.js
@@ -1,12 +1,14 @@
 function firstNonRepeatingLetter(str) {
   let doubleChars = [];
   let currChar;
+  let lwrstr = str.toLowerCase();
   for(let i=0; i<=str.length; i++) {
-    currChar = str.charAt(i);
-    if(str.substring(i+1).indexOf(currChar)==-1 && ! doubleChars.includes(currChar)) {
-      return currChar;
+    currChar = lwrstr.charAt(i);
+    if(lwrstr.substring(i+1).indexOf(currChar)==-1 && ! doubleChars.includes(currChar)) {
+      return str.charAt(i);
     } else if(!doubleChars.includes(currChar)) {
       doubleChars.push(currChar);
     }
   }
+  return '';
 }

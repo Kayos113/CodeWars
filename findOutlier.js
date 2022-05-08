221d35081d20a638a85154d08d8f82722d5b964f
William Knapp <willyknapp@gmail.com>
2022-05-08 14:12:41 -0500

    findOutlier update

--- findOutlier.js
+++ findOutlier.js
@@ -3,10 +3,10 @@ function findOutlier(integers){
   for(let i=0; i<3; i++) {
     count[integers[i]%2]++;
   }
-  let modTarget = count[0]>1 ? 1 : 0; //sets whether we are looking for odd or even values. Greater than 1 in count[0] means there are more evens than odds in this array so we need to look for a value where %2=1, hence making modTarget1
+  let modTarget = count[0]>1 ? 1 : 0;  //sets whether we are looking for odd or even values. Greater than 1 in count[0] means there are more evens than odds in this array so we need to look for a value where %2=1, hence making modTarget1
   let index;
   integers.forEach( (int, i) => {
-    if(int%2==modTarget) {
+    if(Math.abs(int%2)==modTarget) {
       index = i;
     }
   });

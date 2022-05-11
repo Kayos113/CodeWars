281f0153869282b6b507aac945d0b12a4419c1ea
Knapp <williamknapp@rockypoint.k12.ny.us>
2022-05-11 11:12:50 -0400

    createPhoneNumber challenge, 5-11-2022

---
+++ createPhoneNumber.js
@@ -0,0 +1,15 @@
+function createPhoneNumber(numbers){
+  if(numbers.length!=10) {
+    return "INVALID ARRAY FORMAT"
+  }
+  let returnString = "(";
+  for(let i=0; i<numbers.length; i++) {
+    returnString += numbers[i];
+    if(i===2) { //after the third number
+      returnString += ") ";
+    } else if(i===5) { //after the sixth number
+      returnString += "-";
+    }
+  }
+  return returnString;
+}

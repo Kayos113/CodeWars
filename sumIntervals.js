c6b9005e5ab809782a8ad3b6a8062fddd0fd578c
William Knapp <willyknapp@gmail.com>
2022-05-23 11:32:34 -0400

    Codewars 5-23-2022

---
+++ sumIntervals.js
@@ -0,0 +1,32 @@
+function sumIntervals(intervals) {
+  let sumArr = [];
+  intervals.forEach(interval => {
+    if(sumArr.length==0) {
+      sumArr.push(interval);
+    }
+    let incorporated = false;
+    sumArr.forEach( (trueInterval, index) => {
+      if((interval[0] <= trueInterval[1] && interval[0] >= trueInterval[0]) || (interval[1] >= trueInterval[0] && interval[1] <= trueInterval[1]) || (interval[0] <= trueInterval[0] && interval[1] >= trueInterval[1])) {
+        // interval overlaps or encapsulates
+        if(interval[0] < trueInterval[0]) {
+          trueInterval[0] = interval[0];
+        }
+        if(interval[1] > trueInterval[1]) {
+          trueInterval[1] = interval[1];
+        }
+        incorporated = true;
+      }
+      if(index==sumArr.length-1 && !incorporated) {
+        sumArr.push(interval);
+      }
+    })
+  })
+  let sum = 0;
+  sumArr.forEach(interval => {
+    sum += interval[1]-interval[0];
+  })
+  console.log(intervals);
+  console.log(sumArr);
+  console.log(sum);
+  return sum;
+}
